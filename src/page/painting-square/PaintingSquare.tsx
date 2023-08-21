697fa9f6604f7f428d8c00ff20ece989b9a843f3
fary <910146725@qq.com>
2023-08-21 20:20:09 +0800

    update images

--- src/page/painting-square/PaintingSquare.tsx
+++ src/page/painting-square/PaintingSquare.tsx
@@ -1,18 +1,18 @@
 import { faCopy, faDownload } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { Image, Card, Button, message } from 'antd';
+import { Image, Card, Button, message, Divider, Skeleton } from 'antd';
 import Masonry from 'masonry-layout'
 import React, { useEffect, useState } from 'react';
-import VirtualList from 'rc-virtual-list';
 import apiSetting from '../../store/APISetting';
 import imageData, { IImageSharing } from '../../store/ImageData';
 import copy from 'copy-to-clipboard';
 import './PaintingSquare.css'
 import { useTranslation } from 'react-i18next';
-type IProps={
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+type IProps = {
 }
 
-const PaintingSquare:React.FC<IProps> = ()=>{
+const PaintingSquare: React.FC<IProps> = () => {
 
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<IImageSharing[]>([]);
@@ -23,23 +23,23 @@ const PaintingSquare:React.FC<IProps> = ()=>{
   const [prompt, setPrompt] = useState<string>("");
   const [timeoutObj, setTimeoutObj] = useState<any>(null);
 
-  const pageSize=10;
+  const pageSize = 10;
   const loadMoreData = () => {
     if (loading) {
       return;
     }
     setLoading(true);
     fetch(`${apiSetting.imageSharingUrl}?offset=${offset}&pageSize=${pageSize}&prompt=${decodeURI(prompt)}`)
-      .then((res) => {return res.json()})
+      .then((res) => { return res.json() })
       .then((body) => {
         setTotal(body.data.total);
-        const responseData=body.data.data as IImageSharing[];
-        if(responseData){
-          setOffset(offset+responseData.length);
+        const responseData = body.data.data as IImageSharing[];
+        if (responseData) {
+          setOffset(offset + responseData.length);
           responseData.forEach((item) => {
-            item.width=imageData.getWidthBySize(item.size);
-            item.height=imageData.getHeightBySize(item.size);
-            item.urlSmall=item.urlBig+`?imageMogr2/thumbnail/${item.width}/lquality/100`
+            item.width = imageData.getWidthBySize(item.size);
+            item.height = imageData.getHeightBySize(item.size);
+            item.urlSmall = item.urlBig + `?imageMogr2/thumbnail/${item.width}/lquality/100`
           });
           setData([...data, ...responseData]);
         }
@@ -47,22 +47,22 @@ const PaintingSquare:React.FC<IProps> = ()=>{
       })
       .catch(() => {
         setLoading(false);
-      }).finally(()=>{
+      }).finally(() => {
         advanceWidth();
       });
   };
 
 
-  const changePrompt=(prompt:string)=>{
+  const changePrompt = (prompt: string) => {
     setPrompt(prompt);
     console.log(timeoutObj);
-    if(timeoutObj!==null){
+    if (timeoutObj !== null) {
       clearTimeout(timeoutObj);
     }
-    setTimeoutObj(setTimeout(()=>{
+    setTimeoutObj(setTimeout(() => {
       setData([]);
       loadMoreData();
-    },1000));
+    }, 1000));
   }
 
   useEffect(() => {
@@ -70,14 +70,14 @@ const PaintingSquare:React.FC<IProps> = ()=>{
   }, []);
 
 
-  const copyPrompt=(prompt:string)=>{
+  const copyPrompt = (prompt: string) => {
     copy(prompt)
-    messageApi.success(t("Copy prompt successlly"),5);
+    messageApi.success(t("Copy prompt successlly"), 5);
   }
 
-  const getMask=(prompt:string)=>{
+  const getMask = (prompt: string) => {
     return (
-      <div className="image-mask" onClick={(e)=>e.stopPropagation()}>
+      <div className="image-mask" onClick={(e) => e.stopPropagation()}>
         <div className="image-mask-content">
           {prompt}
         </div>
@@ -86,16 +86,16 @@ const PaintingSquare:React.FC<IProps> = ()=>{
 
   const advanceWidth = () => {
     // new Masonry(节点, 配置)
-    setTimeout(()=>{
-      new Masonry(document.querySelector('.painting-square-image-items') as any, {
+    setTimeout(() => {
+      new Masonry(document.getElementById("painting-square-image-items") as any, {
         itemSelector: '.painting-square-image-item-wrapper', // 要布局的网格元素
         columnWidth: 300,  // 获取节点 可以自动计算每列的宽度
         fitWidth: true, // 设置网格容器宽度等于网格宽度
       })
-    },0)
+    }, 0)
   }
 
-  const ContainerHeight = window.innerHeight-91;
+  const ContainerHeight = window.innerHeight - 91;
 
 
   const onScroll = (e: React.UIEvent<HTMLElement, UIEvent>) => {
@@ -107,11 +107,11 @@ const PaintingSquare:React.FC<IProps> = ()=>{
   const buttonStyle = {
     width: "100%",
     fontSize: "14px",
-    color:"#000"
+    color: "#000"
   }
 
 
-    return (
+  return (
     <div className="painting-square-page" id="scrollableDiv">
       {/* <div className="painting-actions">
         <div className="actions-input">
@@ -121,43 +121,49 @@ const PaintingSquare:React.FC<IProps> = ()=>{
         </div>
         <div className="actions-adjust"></div>
       </div> */}
-       <div className="painting-square-image-items">
-      <VirtualList
-        data={data}
-        height={ContainerHeight}
-        itemKey="urlBig"
-        onScroll={onScroll}
+      <InfiniteScroll
+        dataLength={data.length}
+        next={() => { loadMoreData() }}
+        hasMore={data.length < total}
+        loader={<Skeleton avatar paragraph={{ rows: 1 }} active />}
+        endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+        scrollableTarget="scrollableDiv"
       >
-         {(item) => (
-            item.visible!==false && <div className="painting-square-image-item-wrapper">
-              <Card className="painting-square-image-item" 
-              actions={[
-                <Button icon={<FontAwesomeIcon icon={faCopy} />}
-                  style={buttonStyle} type="link" onClick={() => { copyPrompt(item.prompt); }}>
-                  &nbsp;{t('copy prompt')}</Button>,
-                <Button  style={buttonStyle}
-                onClick={()=>imageData.downloadImage(item.urlBig)} type="link"
-                icon={<FontAwesomeIcon icon={faDownload} />}>{t('download')}</Button>
-            
-              ]}>
-              <Image src={item.urlSmall} 
-                  width={item.width} 
-                  height={item.height} 
+        <div id="painting-square-image-items" className="painting-square-image-items">
+          {
+            data.map((item,index) => (
+              <div key={index} className="painting-square-image-item-wrapper">
+              <Card   className="painting-square-image-item"
+                actions={[
+                  <Button icon={<FontAwesomeIcon icon={faCopy} />}
+                    style={buttonStyle} type="link" onClick={() => { copyPrompt(item.prompt); }}>
+                    &nbsp;{t('copy prompt')}</Button>,
+                  <Button style={buttonStyle}
+                    onClick={() => imageData.downloadImage(item.urlBig)} type="link"
+                    icon={<FontAwesomeIcon icon={faDownload} />}>{t('download')}</Button>
+
+                ]}>
+                <Image src={item.urlSmall}
+                  width={item.width}
+                  height={item.height}
                   fallback={"https://image-1257149217.cos.ap-guangzhou.myqcloud.com/error/404.png"}
-                  onError={()=>item.visible=false}
+                  onError={() => item.visible = false}
                   preview={
-                    {src:item.urlBig,
-                    mask:getMask(item.prompt)}
+                    {
+                      src: item.urlBig,
+                      mask: getMask(item.prompt)
+                    }
                   }
-                  ></Image>
-                  </Card>
-                </div>)
-         }
-      </VirtualList>
-      </div>
-        {contextHolder}
-      </div>
-    )
+                ></Image>
+              </Card>
+              </div>
+            ))
+          }
+                      </div>
+      </InfiniteScroll>
+      {contextHolder}
+    </div>
+  )
 }
 
 export default PaintingSquare;
