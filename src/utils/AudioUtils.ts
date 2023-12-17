83bbcaebe1f3b28f66b0dbebc4c830503c562086
faryhuo <910146725@qq.com>
2023-12-17 23:10:33 +0800

    draw interface

--- src/utils/AudioUtils.ts
+++ src/utils/AudioUtils.ts
@@ -10,7 +10,48 @@ export function playAudio(stream,msgFun,fun:(data)=>void,stopEvent:()=>void){
     const audioChunks = [];
     const currentDate=new Date();
     let mediaRecorder = new MediaRecorder(stream);
+    let audioContext= new AudioContext();
+    const source=audioContext.createMediaStreamSource(stream);
+    const analyser = audioContext.createAnalyser();
+    source.connect(analyser);
+    const canvas = document.createElement("canvas") as HTMLCanvasElement;
+
+    document.appendChild(canvas)
+    const canvasContext = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    function draw() {
+      // 获取音频数据
+      const bufferLength = analyser.frequencyBinCount;
+      const dataArray = new Uint8Array(bufferLength);
+      analyser.getByteFrequencyData(dataArray);
+    
+      // 清空Canvas
+      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
+    
+      // 绘制音频动画
+      const barWidth = (canvas.width / bufferLength) * 2.5;
+      let barHeight;
+      let x = 0;
+    
+      for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+    
+        canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+        canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+    
+        x += barWidth + 1;
+      }
+    
+      // 循环绘制动画
+      requestAnimationFrame(draw);
+    }
+    draw();
     mediaRecorder.ondataavailable = (event) => {
+      analyser.fftSize=512;
+      const buffers=new Uint8Array(analyser.frequencyBinCount);
+      analyser.getByteFrequencyData(buffers);
+      console.log(buffers);
       audioChunks.push(event.data);
     };
 
@@ -19,6 +60,7 @@ export function playAudio(stream,msgFun,fun:(data)=>void,stopEvent:()=>void){
         msgFun('audio cannot less than 3s. please hold on button to record.')
         return;
       }
+      source.disconnect();
       // 使用Blob将音频片段转成一个音频文件
       const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
       // 音频文件处理，例如上传或者本地保存
