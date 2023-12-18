310104c9fbe45e205e9d82da4c814cc9856b7ab8
fary <910146725@qq.com>
2023-12-18 18:09:41 +0800

    audio enhancement

--- src/component/send-button/SendButton.tsx
+++ src/component/send-button/SendButton.tsx
@@ -1,14 +1,14 @@
 import { useEffect, useRef } from 'react';
 import { observer } from "mobx-react-lite";
 import { useState } from 'react';
-import { Input, Space, Button,message as messageApi } from 'antd';
+import { Input, Space, Button, message as messageApi } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPaperPlane, faBroom, faMicrophone, faThumbTack, faRecordVinyl, faKeyboard } from '@fortawesome/free-solid-svg-icons'
 import { useTranslation } from 'react-i18next';
 import './SendButton.css'
 import { IAppConfig } from '../../store/AppConfig';
 import { IMessage } from '../../store/MessageData';
-import {  fetchAudio, getMediaStream, playAudio, stopAudio } from '../../utils/AudioUtils';
+import { callTranscations, getMediaStream, mediaRecorder, playAudio, stopAudio } from '../../utils/AudioUtils';
 import Upload from '../upload/Upload';
 
 type IProps = {
@@ -19,7 +19,8 @@ type IProps = {
 
 
 const { TextArea } = Input;
-
+let audioObj=null;
+let isCancel=false;
 const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) => {
   const [message, setMessage] = useState("");
   const [minRow, setMinRow] = useState(2);
@@ -32,51 +33,29 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
     setBtnHeight((btnRef?.current as any)?.getBoundingClientRect()?.height + 5);
 
   }
-  const [audioObj, setAudioObj] = useState(null);
-  const [timeObj, setTimeobj] = useState(null);
-  const [inputModel, setInputModel] = useState("keyborad");
+  const timeObj = useRef(null);
+  const [inputModel, setInputModel] = useState<'keyboard' | 'audio'>("keyboard");
   const [recordStartime, setRecordStartime] = useState(null);
   const [recordEndTime, setRecordEndTime] = useState(null);
-  let timerObj=null;
+  let fetchAudio = useRef(false);
+  let [fetchAudioState,setFetchAudioSate] = useState(false);
 
-  const closeAudio=()=>{
-    stopAudio(audioObj);
-    setStart(false);
-    setAudioObj(null);
-  }
+  let timerObj = useRef(null);
 
-  const getDuringTime=()=>{
-    if(!recordEndTime || !recordStartime){
-      return 0;
-    }
-    return recordEndTime.getTime()-recordStartime.getTime()
+  const closeAudio = () => {
+    mediaRecorder && mediaRecorder.stop();
+    audioObj && stopAudio(audioObj);
   }
 
 
-
-  // const playAudio=()=>{
-  //   setStart(true);
-  //   getMediaStream().then((stream)=>{
-  //     setAudioObj(stream);
-  //     playAudio(stream,(data)=>{
-  //       //setMessage(data.text);
-  //       if(data.text){
-  //         sentMsgToChat(data.text);
-  //       }else{
-
-  //       }
-  //     },closeAudio)
-  //   })
-  // }
-
-  const convertInputModel=()=>{
-    if(inputModel==='keyborad'){
+  const convertInputModel = () => {
+    if (inputModel === 'keyboard') {
       setMinRow(2);
       setTimeout(() => {
         setInputModel('audio');
       });
-    }else{
-      setInputModel('keyborad');
+    } else {
+      setInputModel('keyboard');
     }
   }
 
@@ -97,25 +76,25 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
   }
 
   function secondsToHMS(seconds) {
-   
-  seconds=seconds/1000;
-  // 计算小时、分钟和秒
-  var hours = Math.floor(seconds / 3600);
-  var minutes = Math.floor((seconds % 3600) / 60);
-  var remainingSeconds = Math.floor(seconds % 60);
 
-  // 格式化成两位数
-  var formattedHours = ('0' + hours).slice(-2);
-  var formattedMinutes = ('0' + minutes).slice(-2);
-  var formattedSeconds = ('0' + remainingSeconds).slice(-2);
+    seconds = seconds / 1000;
+    // 计算小时、分钟和秒
+    var hours = Math.floor(seconds / 3600);
+    var minutes = Math.floor((seconds % 3600) / 60);
+    var remainingSeconds = Math.floor(seconds % 60);
 
-  // 构造时间字符串
-  var timeString = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds;
+    // 格式化成两位数
+    var formattedHours = ('0' + hours).slice(-2);
+    var formattedMinutes = ('0' + minutes).slice(-2);
+    var formattedSeconds = ('0' + remainingSeconds).slice(-2);
 
-  return timeString;
-}
+    // 构造时间字符串
+    var timeString = formattedHours + ':' + formattedMinutes + ':' + formattedSeconds;
 
-  const sentMsgToChat=(message)=>{
+    return timeString;
+  }
+
+  const sentMsgToChat = (message) => {
     let chatId = store.activeSession + "";
     chatId = store.checkChatId(chatId);
     store.addData({
@@ -132,34 +111,88 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
     })
   }
 
-  useEffect(()=>{
+
+  const handleAudio = (isStart) => {
     if (!isStart) {
-      if(timeObj){
-        clearTimeout(timeObj);
-        setTimeobj(null);
+      if (timeObj.current) {
+        clearTimeout(timeObj.current);
+        timeObj.current=null;
       }
-      clearInterval(timerObj);
+      fetchAudio.current=false;
+      setFetchAudioSate(false);
       closeAudio();
     } else {
-      setRecordStartime(new Date());
-      if(timerObj===null){
-        timerObj=setInterval(()=>{
-          setRecordEndTime(new Date());
-        },100)
+      if (fetchAudio.current) {
+        return;
       }
-      getMediaStream().then((stream)=>{
-        setAudioObj(stream);
-        playAudio(stream,messageFun.error,(data)=>{
-          //setMessage(data.text);
-          if(data.text){
-            sentMsgToChat(data.text);
-          }else{
-
+      const startTime = new Date();
+      fetchAudio.current=true;
+      setFetchAudioSate(true);
+      getMediaStream().then((stream) => {
+        audioObj=stream;
+        playAudio(stream).then((audio: any) => {
+          if (isCancel) {
+            isCancel=false;
+            fetchAudio.current=false;
+            setFetchAudioSate(false);
+            return;
           }
-        },closeAudio)
+          if (new Date().getTime() - startTime.getTime() < 3000) {
+            messageFun.error(t('audio cannot less than 3s. please hold on button to record.'));
+            fetchAudio.current=false;
+            setFetchAudioSate(false);
+            return;
+          }
+          callTranscations(audio).then((data) => {
+            const json = JSON.parse(data.data);
+            if (json.text) {
+              sentMsgToChat(json.text);
+            } else {
+              messageFun.error(t('Audio to text failed'));
+            }
+          }).finally(() => {
+            fetchAudio.current=false;
+            setFetchAudioSate(false);
+          })
+        }, (errMsg) => {
+          fetchAudio.current=false;
+          setFetchAudioSate(false);
+          if (isCancel) {
+            isCancel=false;
+            return;
+          }
+          if (errMsg) {
+            messageFun.error(errMsg);
+          }
+        }).finally(() => {
+          closeAudio();
+        })
+        timeObj.current=setTimeout(() => {
+          mediaRecorder && mediaRecorder.stop();
+          stopAudio(stream);
+        },60000);
       })
     }
+  }
+
+  useEffect(()=>{
+    if(isStart){
+      const startTime=new Date();
+      setRecordStartime(startTime)
+      setRecordEndTime(new Date());
+      if (timerObj.current === null) {
+        timerObj.current = setInterval(() => {
+          setRecordEndTime(new Date());
+        }, 100)
+      }
+    }else{
+      if(timerObj.current){
+        clearInterval(timerObj.current);
+        timerObj.current=null;
+      }
+    }
   },[isStart])
+
 
   const typeMsg = (e: any) => {
     setMessage(e.target.value)
@@ -171,28 +204,34 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
   }
 
   const handleMouseDown = () => {
-    if(fetchAudio!=null){
-      return;
-    }
     if (store.isType === false) {
       return
     }
-    setStart(true);
+    if (fetchAudio.current) {
+      return
+    }
+    if(isStart===false){
+      setStart(true);
+      handleAudio(true);
+    }
   };
 
   const handleMouseUp = () => {
-    setStart(false);
+    if(isStart===true){
+      setStart(false);
+      handleAudio(false);
+    }
   };
 
   return (<div ref={btnRef}>
     <Space.Compact style={{ width: '100%' }}>
-      <div className="upload-btn-wrapper">
-      {/* <Button className='upload-btn'  disabled={store.isType===false}  shape="circle"
+      {inputModel === 'keyboard' && <div className="upload-btn-wrapper">
+        <Button className='upload-btn' disabled={store.isType === false} shape="circle"
           icon={
             <Upload store={store} config={config}   ></Upload>
-          }/> */}
-      </div>
-      {inputModel==='keyborad'?<TextArea
+          } />
+      </div>}
+      {inputModel === 'keyboard' ? <TextArea
         value={message}
         placeholder={t("Type here... (Shift + Enter = Line break)") as string}
         size="large"
@@ -202,20 +241,21 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
         onResize={resizeHeight}
         spellCheck={true}
         allowClear
-      />:<div className='recorder-input-wrapper'>
-        {isStart && <span className='recorder-timer'><FontAwesomeIcon icon={faRecordVinyl}/>{recordStartime && recordEndTime && secondsToHMS(recordEndTime.getTime()-recordStartime.getTime())}</span>}
-      <Button disabled={store.isType === false || fetchAudio!==null}  size='large' icon={<FontAwesomeIcon size='lg' icon={faMicrophone}/>} 
-      onMouseDown={handleMouseDown}
-      onTouchStart={handleMouseDown}
-      onTouchEnd={handleMouseUp}
-      onMouseUp={handleMouseUp}
-      // onMouseLeave={handleMouseUp} // 处理鼠标在按钮上按住然后离开按钮的情况
-      style={{width:'100%',height:'65px',color:isStart?'red':'black'}} 
-    >{t('Press and hold the button to record')}</Button></div>}
+      /> : <div className='recorder-input-wrapper'>
+        {isStart && <span className='recorder-timer'><FontAwesomeIcon icon={faRecordVinyl} />{recordStartime && recordEndTime && secondsToHMS(recordEndTime.getTime() - recordStartime.getTime())}</span>}
+        <Button disabled={store.isType === false || fetchAudioState} size='large' icon={<FontAwesomeIcon size='lg' icon={faMicrophone} />}
+          onMouseDown={handleMouseDown}
+          onTouchStart={handleMouseDown}
+          onTouchEnd={handleMouseUp}
+          onMouseUp={handleMouseUp}
+          onClick={(e)=>e.preventDefault()}
+          // onMouseLeave={handleMouseUp} // 处理鼠标在按钮上按住然后离开按钮的情况
+          style={{ width: '100%', height: '65px', borderRadius: 20, color: isStart ? 'red' : 'black' }}
+        >{t('Press and hold the button to record')}</Button></div>}
       <div className="sent-btn-actions"  >
         <Button disabled={store.isType === false} shape="circle" onClick={convertInputModel}
-          icon={inputModel!=='keyborad' ? <FontAwesomeIcon icon={faKeyboard} /> : <FontAwesomeIcon icon={faMicrophone} />} />
-        
+          icon={inputModel !== 'keyboard' ? <FontAwesomeIcon icon={faKeyboard} /> : <FontAwesomeIcon icon={faMicrophone} />} />
+
         <Button className="resize-btn" shape="circle" icon={<FontAwesomeIcon icon={faBroom} />}
           onClick={() => { store.addChat() }}></Button>
       </div>
@@ -226,6 +266,21 @@ const SendButton: React.FC<IProps> = observer(({ store, config, setBtnHeight }) 
       </div>
     </Space.Compact>
     {contextHolder}
+    {isStart && <div
+      onMouseUp={handleMouseUp}
+      onTouchEnd={handleMouseUp}
+      className='audio-wrapper'>
+      <div className="canvas-wrapper">
+        <canvas id='canvas'></canvas>
+      </div>
+      <div className="audio-inputs">
+        <Button shape='circle'
+          onMouseEnter={() => { isCancel=true; }}
+          onMouseLeave={() => { isCancel=false;}}
+        >{t('Cancel')}</Button>
+        <Button shape='circle'>{t('Text')}</Button>
+      </div>
+    </div>}
   </div>);
 });
 
--- src/utils/AudioUtils.ts
+++ src/utils/AudioUtils.ts
@@ -1,97 +1,82 @@
 import apiSetting from "../store/APISetting";
-import i18n from "./i18n";
 export function getMediaStream() {
     return navigator.mediaDevices.getUserMedia({ audio: true, video: false });
 }
 
-export let fetchAudio=null;
+function draw(analyser,canvas) {
+  // 获取音频数据
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+  analyser.getByteFrequencyData(dataArray);
+  const canvasContext = canvas.getContext('2d');
+  // 清空Canvas
+  canvasContext.clearRect(0, 0, canvas.width, canvas.height);
 
-export function playAudio(stream,msgFun,fun:(data)=>void,stopEvent:()=>void){
-    const audioChunks = [];
-    const currentDate=new Date();
-    let mediaRecorder = new MediaRecorder(stream);
-    let audioContext= new AudioContext();
-    const source=audioContext.createMediaStreamSource(stream);
-    const analyser = audioContext.createAnalyser();
-    source.connect(analyser);
-    const canvas = document.createElement("canvas") as HTMLCanvasElement;
+  // 绘制音频动画
+  const barWidth = (canvas.width / bufferLength) * 2.5;
+  let barHeight;
+  let x = 0;
 
-    document.appendChild(canvas)
-    const canvasContext = canvas.getContext('2d');
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
-    function draw() {
-      // 获取音频数据
-      const bufferLength = analyser.frequencyBinCount;
-      const dataArray = new Uint8Array(bufferLength);
-      analyser.getByteFrequencyData(dataArray);
-    
-      // 清空Canvas
-      canvasContext.clearRect(0, 0, canvas.width, canvas.height);
-    
-      // 绘制音频动画
-      const barWidth = (canvas.width / bufferLength) * 2.5;
-      let barHeight;
-      let x = 0;
-    
-      for (let i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-    
-        canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
-        canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-    
-        x += barWidth + 1;
+  for (let i = 0; i < bufferLength; i++) {
+    barHeight = dataArray[i];
+
+    canvasContext.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+    canvasContext.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+
+    x += barWidth + 1;
+  }
+
+  // 循环绘制动画
+  requestAnimationFrame(()=>draw(analyser,canvas));
+}
+
+export let mediaRecorder;
+
+export function playAudio(stream){
+    const promise=new Promise((resolve,reject)=>{
+      const audioChunks = [];
+      mediaRecorder = new MediaRecorder(stream);
+      let audioContext= new AudioContext();
+      const source=audioContext.createMediaStreamSource(stream);
+      const analyser = audioContext.createAnalyser();
+      try{
+        source.connect(analyser);
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        draw(analyser,canvas);
+      }catch(e){
+        source.disconnect();
       }
-    
-      // 循环绘制动画
-      requestAnimationFrame(draw);
-    }
-    draw();
-    mediaRecorder.ondataavailable = (event) => {
-      analyser.fftSize=512;
-      const buffers=new Uint8Array(analyser.frequencyBinCount);
-      analyser.getByteFrequencyData(buffers);
-      console.log(buffers);
-      audioChunks.push(event.data);
-    };
-
-    mediaRecorder.onstop = () => {
-      if(new Date().getTime()-currentDate.getTime()<3000){
-        msgFun('audio cannot less than 3s. please hold on button to record.')
-        return;
-      }
-      source.disconnect();
-      // 使用Blob将音频片段转成一个音频文件
-      const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
-      // 音频文件处理，例如上传或者本地保存
-      const formData = new FormData();
-      formData.append('file', audioBlob,'recording.webm');
-      formData.append('language', "zh");
-      fetchAudio=fetch(apiSetting.transcriptionsUrl,{
-        method: 'POST',
-        body: formData
-      }).then(response => response.json()).then(data => {
-        fetchAudio=null;
-        if(data.status===0){
-            const json=JSON.parse(data.data);
-            fun(json);
-        }else{
-          msgFun(i18n.t("Fail to call audio transcriptions API"));
-        }
-      })
-      
-    };
-    mediaRecorder.start(); // 开始录音
-    
-    setTimeout(() => {
-        mediaRecorder.stop();
-        stopAudio(stream);
-        stopEvent()
-    },60000);
+      mediaRecorder.ondataavailable = (event) => {
+        analyser.fftSize=512;
+        const buffers=new Uint8Array(analyser.frequencyBinCount);
+        analyser.getByteFrequencyData(buffers);
+        audioChunks.push(event.data);
+      };
+  
+      mediaRecorder.onstop = () => {
+        source.disconnect();
+        // 使用Blob将音频片段转成一个音频文件
+        const audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+        // 音频文件处理，例如上传或者本地保存
+        resolve(audioBlob);
+      };
+      mediaRecorder.start(); // 开始录音
+    })
+    return promise;
 }
 export function stopAudio(stream){
     if (stream) {
         // 关闭所有轨迹以停止使用麦克风
         stream.getTracks().forEach(track => track.stop());
     }
+}
+
+export function callTranscations(audioBlob:Blob){
+  const formData = new FormData();
+  formData.append('file', audioBlob,'recording.webm');
+  formData.append('language', "zh");
+  return fetch(apiSetting.transcriptionsUrl,{
+    method: 'POST',
+    body: formData
+  }).then(response => response.json());
 }
